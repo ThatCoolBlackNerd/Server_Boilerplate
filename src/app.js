22cb19ea62290c6da343b98736d466cd27c561da
ThatCoolBlackNerd <50676581+ThatCoolBlackNerd@users.noreply.github.com>
2020-03-02 23:45:56 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,15 +19,16 @@ app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
 })
 
-+ app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-    })
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+     console.error(error)
+      response = { message: error.message, error }
+  }
+       
+  res.status(500).json(response)
+})
 
 module.exports = app
