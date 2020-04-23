1520851b0d085307042563e4de3136b618991e63
Brandon Carr <bmcarr21@me.com>
2020-04-22 20:23:25 -0500

    Updated Boilerplate with knex

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'Database URL HERE'
+  },
+  test: {
+    client: 'pg',
+    connection: 'Test Database URL HERE'
+  },
+
+};
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const bodyParser = require('body-parser');
 
 const app = express()
 
@@ -14,20 +15,21 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!')
+app.use(/*'/api/v1/endpoint, route file*/);
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+     console.error(error)
+      response = { message: error.message, error }
+  }
+       
+  res.status(500).json(response)
 })
-
-+ app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-    })
 
 module.exports = app
